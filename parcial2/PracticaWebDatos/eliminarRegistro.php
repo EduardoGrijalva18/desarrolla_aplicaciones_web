aa3448534035a371b1b449449b1591b6549b2d5d
EduardoGrijalva18 <99852224+EduardoGrijalva18@users.noreply.github.com>
2022-11-04 17:11:23 -0700

    Update eliminarRegistro.php

--- parcial2/PracticaWebDatos/eliminarRegistro.php
+++ parcial2/PracticaWebDatos/eliminarRegistro.php
@@ -17,3 +17,8 @@ if($conexion->query($sql) === TRUE){
 
 
 ?>
+
+<br><br><br><br>
+<?php
+  echo '<img src="https://th.bing.com/th/id/R.9f93a381794ff81ceabb5431dc468b88?rik=wAuTEjU9LBoGZg&riu=http%3a%2f%2fst.depositphotos.com%2f1001911%2f1554%2fv%2f950%2fdepositphotos_15540341-Thumb-up-emoticon.jpg&ehk=jMHZxwvAa%2bOgsfRzDRy7IbfmPbb%2fInUJF01HG0tsiE4%3d&risl=&pid=ImgRaw&r=0" alt="" class="Imagen" id="EmojiLike" width="600px" height="600px">';
+?>
